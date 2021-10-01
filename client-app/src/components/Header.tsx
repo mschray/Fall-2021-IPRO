07f410748387227313a404eb7c1a2565731dd5d2
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2021-10-01 15:56:46 -0500

    Implement router structure

---
+++ client-app/src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>Jebra</h1>
+            <h2>Interactive Algebra Lessons</h2>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/options">Options</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Log in</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
