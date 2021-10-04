164fd6d80e241ecaaa92beac8a4192f13156fb91
Dan Tiberi <danny.tiberi@gmail.com>
2021-10-03 23:17:01 -0500

    Implemented CustomNonQuery AZ Function

---
+++ JebraAzureFunctions/JebraAzureFunctions/CustomNonQuery.cs
@@ -0,0 +1,45 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using JebraAzureFunctions.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace JebraAzureFunctions
+{
+    public static class CustomNonQuery
+    {
+        [FunctionName("CustomNonQuery")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "General Request" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiRequestBodyAttribute(contentType: "string", bodyType: typeof(CustomNonQueryModel))]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string command = req.Query["command"];
+            //string command = "";
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            command = command ?? data?.command;
+
+            Tools.ExecuteNonQueryAsync(command);
+
+            string responseMessage = $"COMMAND: {command} \n Request Sent.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
---
+++ JebraAzureFunctions/JebraAzureFunctions/Models/CustomNonQueryModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JebraAzureFunctions.Models
+{
+    class CustomNonQueryModel
+    {
+        [JsonProperty]
+        public string command { get; set; }
+    }
+}
