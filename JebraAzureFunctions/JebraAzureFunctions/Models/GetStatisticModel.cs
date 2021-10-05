c428322dade3fc8a99531d5f7fadbcb9658acdd2
Dan Tiberi <danny.tiberi@gmail.com>
2021-10-05 17:34:53 -0500

    Create GetStatisticModel.cs

---
+++ JebraAzureFunctions/JebraAzureFunctions/Models/GetStatisticModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace JebraAzureFunctions.Models
+{
+    class GetStatisticModel
+    {
+        [JsonProperty]
+        public int user_id { get; set; }
+        [JsonProperty]
+        public int course_id { get; set; }
+        [JsonProperty]
+        public int stage_id { get; set; }
+    }
+}
