fb3701b86e762c4e6585d5440b78b2157c4d064e
Dan Tiberi <danny.tiberi@gmail.com>
2021-10-05 18:21:40 -0500

    Create PublishStageEventModel.cs

---
+++ JebraAzureFunctions/JebraAzureFunctions/Models/PublishStageEventModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JebraAzureFunctions.Models
+{
+    class PublishStageEventModel
+    {
+        [JsonProperty]
+        public int stage_id { get; set; }
+        [JsonProperty]
+        public int course_id { get; set; }
+        [JsonProperty]
+        public int origin_user_id { get; set; }
+        [JsonProperty]
+        public int question_id { get; set; }
+        [JsonProperty]
+        public int inflicted_hp { get; set; }
+        [JsonProperty]
+        public bool was_correct { get; set; }
+        [JsonProperty]
+        public string event_time { get; set; }
+    }
+}
