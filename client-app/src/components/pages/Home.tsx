1c3d1d716efe8d46bd8a9588299484a49b9c61a6
Dan Tiberi <danny.tiberi@gmail.com>
2021-10-12 16:00:54 -0500

    Added API example to Home.tsx.

--- client-app/src/components/pages/Home.tsx
+++ client-app/src/components/pages/Home.tsx
@@ -1,8 +1,25 @@
 import React from "react";
+import { setConstantValue } from "typescript";
 
 const Home: React.FC = () => {
+    const [data, setData] = React.useState("");
+    let questionID = 84;
+    React.useEffect(() => {      
+        const fetchData = async () => {
+            var request = "https://jebrafunctions.azurewebsites.net/api/GetQuestion?code=KoWO/aWjlpHbXBep0eePd39TA1uwcTXjYQxMIOaHDgrTwTKkBT3S2g==&id="+questionID;
+            const response = await fetch(request);
+            const json = await response.json();
+            console.log(json);
+            setData(json);
+        }
+        fetchData();
+    }, [])
+
     return (
-        <h3>Home</h3>
+        <div>
+            <h3>Home</h3>
+            <p>Example API request: {JSON.stringify(data)}</p>
+        </div>
     );
 };
 
