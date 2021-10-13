6fa0b99fe2eb23e2e0455bb6dd5254e999bd0941
Dan Tiberi <danny.tiberi@gmail.com>
2021-10-12 21:27:25 -0500

    Create GetSubjects.cs

---
+++ JebraAzureFunctions/JebraAzureFunctions/GetSubjects.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Data.SqlClient;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace JebraAzureFunctions
+{
+    public static class GetSubjects
+    {
+        [FunctionName("GetSubjects")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "General Request" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            string responseMessage = "";
+
+            var str = Environment.GetEnvironmentVariable("SqlConnectionString");
+            using (SqlConnection conn = new SqlConnection(str))
+            {
+                conn.Open();
+
+                var command = $"SELECT * FROM subject";
+
+                using (SqlCommand cmd = new SqlCommand(command, conn))
+                {
+                    SqlDataReader rows = await cmd.ExecuteReaderAsync();
+
+                    responseMessage = Tools.SqlDatoToJson(rows);//Convert object to JSON.
+                }
+            }
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
