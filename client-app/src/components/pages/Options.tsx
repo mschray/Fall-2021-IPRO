19c6f82dd626828df4ac3866f078ab312977a667
nnguyen17 <nnguyen17@hawk.iit.edu>
2021-11-01 21:56:07 -0500

    test new Student component

--- client-app/src/components/pages/Options.tsx
+++ client-app/src/components/pages/Options.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-import styles from "./Page.module.scss"
+import styles from "./Page.module.scss";
+
+import Student from "components/Student";
 
 const Options: React.FC = () => {
     return (
         <div className={styles.content}>
             <h3>Options</h3>
+            <Student code="code=773202" />
         </div>
     );
 };
