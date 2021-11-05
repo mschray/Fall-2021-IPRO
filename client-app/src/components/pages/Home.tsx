6ea6934dee0c772e924df7bfe7bb6e5bb748fe67
Isaac Kane <ikane@hawk.iit.edu>
2021-11-05 14:56:53 -0500

    Created the overall flexboxes for the home page.

--- client-app/src/components/pages/Home.tsx
+++ client-app/src/components/pages/Home.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Page.module.scss";
+import homeStyles from "./Home.module.scss";
 
 const Home: React.FC = () => {
     return (
         <div className={styles.content}>
             <h3>Home</h3>
-            <ul className={styles.container}>
-                <li><Link to="/studentlogin">Student login</Link></li>
-                <li><Link to="/login">Instructor login</Link></li>
-            </ul>
+            <div className={homeStyles.overallContainer}>
+                <div className={styles.container}>
+                    <h1>Your Stats</h1>
+                </div>
+                <div className={styles.container}>
+                    <h1>Available Subjects</h1>
+                </div>
+                <ul className={styles.container}>
+                    <li><Link to="/studentlogin">Student login</Link></li>
+                    <li><Link to="/login">Instructor login</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
