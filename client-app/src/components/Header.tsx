ff801f65886549a5cf283be4d468fd950f1db8b9
a-cordell <acordell@hawk.iit.edu>
2021-11-09 19:27:19 -0600

    Changed header to be responsive

--- client-app/src/components/Header.tsx
+++ client-app/src/components/Header.tsx
@@ -1,15 +1,27 @@
-import React from "react";
-
 import styles from "./Header.module.scss"
 import { ReactComponent as JebraSVG } from "assets/JebraLogov2Dark.svg";
 
-import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import { StyledEngineProvider } from "@mui/material/styles"
 
-const headersData = [
+import { StyledEngineProvider } from "@mui/material/styles"
+import { styled } from '@mui/material/styles';
+import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState, useEffect } from "react";
+
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    makeStyles,
+    Button,
+    IconButton,
+    Drawer,
+    Link,
+    MenuItem,
+  } from "@material-ui/core";
+  import { Link as RouterLink } from "react-router-dom";
+import { height, margin } from "@mui/system";
+
+  const headersData = [
     {
         label: "Home",
         href: "/",
@@ -26,34 +38,151 @@ const headersData = [
         label: "Play",
         href: "/play",
     },
-];
+  ];
+  
+  const useStyles = makeStyles(() => ({
+    header: {
+      backgroundColor: "#262F34",
+      filter: "drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.25))",
+      paddingRight: "79px",
+      paddingLeft: "5rem",
+      "@media (max-width: 900px)": {
+        paddingLeft: "5rem",
+      },
+      borderRadius: "15px",
+      display: "flex",
+      flexFlow:"row wrap",
+      justifyContent: "space-between",
 
-
-const Header: React.FC = () => {
-    const menuButtons = headersData.map(({ label, href }) => (
-        <Button
-            className={styles.menuButton}
-            key={label}
-            color="inherit"
-            to={href}
-            component={Link}
-        >
+      position: "relative",
+      height: "5rem"
+    },
+    menuButton: {
+      fontFamily: "Open Sans, sans-serif",
+      fontWeight: 700,
+      size: "18px",
+      marginLeft: "38px",
+    },
+    toolbar: {
+      display: "flex",
+      justifyContent: "space-between",
+    },
+    drawerContainer: {
+      padding: "20px 30px",
+    },
+  }));
+  
+  export default function Header() {
+    const { header, menuButton, toolbar, drawerContainer } = useStyles();
+  
+    const [state, setState] = useState({
+      mobileView: false,
+      drawerOpen: false,
+    });
+  
+    const { mobileView, drawerOpen } = state;
+  
+    useEffect(() => {
+      const setResponsiveness = () => {
+        return window.innerWidth < 900
+          ? setState((prevState) => ({ ...prevState, mobileView: true }))
+          : setState((prevState) => ({ ...prevState, mobileView: false }));
+      };
+  
+      setResponsiveness();
+  
+      window.addEventListener("resize", () => setResponsiveness());
+  
+      return () => {
+        window.removeEventListener("resize", () => setResponsiveness());
+      };
+    }, []);
+  
+    const displayDesktop = () => {
+      return (
+        <Toolbar className={toolbar}>
+          <JebraSVG className={styles.logoDesktop} />
+          <div>{getMenuButtons()}</div>
+        </Toolbar>
+      );
+    };
+  
+    const displayMobile = () => {
+      const handleDrawerOpen = () =>
+        setState((prevState) => ({ ...prevState, drawerOpen: true }));
+      const handleDrawerClose = () =>
+        setState((prevState) => ({ ...prevState, drawerOpen: false }));
+  
+      return (
+        <Toolbar>
+          <IconButton
+            {...{
+              edge: "start",
+              color: "inherit",
+              "aria-label": "menu",
+              "aria-haspopup": "true",
+              onClick: handleDrawerOpen,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+  
+          <Drawer
+            {...{
+              anchor: "left",
+              open: drawerOpen,
+              onClose: handleDrawerClose,
+            }}
+          >
+            <div className={drawerContainer}>{getDrawerChoices()}</div>
+          </Drawer>
+  
+          <JebraSVG className={styles.logoMobile} />
+        </Toolbar>
+      );
+    };
+  
+    const getDrawerChoices = () => {
+      return headersData.map(({ label, href }) => {
+        return (
+          <Link
+            {...{
+              component: RouterLink,
+              to: href,
+              color: "inherit",
+              style: { textDecoration: "none" },
+              key: label,
+            }}
+          >
+            <MenuItem>{label}</MenuItem>
+          </Link>
+        );
+      });
+    };
+  
+    const getMenuButtons = () => {
+      return headersData.map(({ label, href }) => {
+        return (
+          <Button
+            {...{
+              key: label,
+              color: "inherit",
+              to: href,
+              component: RouterLink,
+              className: menuButton,
+            }}
+          >
             {label}
-        </Button>
-    ));
-
+          </Button>
+        );
+      });
+    };
+  
     return (
-        <StyledEngineProvider injectFirst>
-            <AppBar className={styles.header}>
-                <JebraSVG className={styles.logo} />
-                <Toolbar className={styles.toolbar}>
-                    <div className={styles.menu}>
-                        {menuButtons}
-                    </div>
-                </Toolbar>
-            </AppBar>
-        </StyledEngineProvider>
+      <header>
+        <AppBar className={header}>
+          {mobileView ? displayMobile() : displayDesktop()}
+        </AppBar>
+      </header>
     );
-};
-
-export default Header;
+  }
