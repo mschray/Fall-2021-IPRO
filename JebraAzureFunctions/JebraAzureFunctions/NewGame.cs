16adc650c0d8790b8babc712a09724ac8555ced4
Dan Tiberi <danny.tiberi@gmail.com>
2021-11-10 11:51:37 -0600

    Create NewGame.cs

---
+++ JebraAzureFunctions/JebraAzureFunctions/NewGame.cs
@@ -0,0 +1,35 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace JebraAzureFunctions
+{
+    public static class NewGame
+    {
+        [FunctionName("NewGame")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "subjectName", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **subjectName** parameter")]
+        [OpenApiParameter(name: "instructorEmail", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **instructorEmail** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            string name = req.Query["name"];
+
+            string responseMessage = "WORK IN PROGRESS";
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
