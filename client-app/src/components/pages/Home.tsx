91c6bd1558b8e0086657f93ab947c7c8e41c7463
Isaac Kane <ikane@hawk.iit.edu>
2021-11-13 20:27:50 -0600

    Get the list of subjects on the home page

--- client-app/src/components/pages/Home.tsx
+++ client-app/src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Page.module.scss";
 import homeStyles from "./Home.module.scss";
@@ -6,16 +6,57 @@ import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import CreateIcon from "@mui/icons-material/Create";
 
+import getAzureFunctions from "getAzureFunctions";
+import useFetch, { FetchStatus } from "hooks/useFetch";
+import { isSubjectModel } from "models/SubjectModel";
+
+const SubjectSelector: React.FC<{ callback: (name: string) => void }> = (props) => {
+    // Fetch the subjects
+    const fetchResult = useFetch(
+        getAzureFunctions().GetSubjects,
+        (data) => {
+            // The Azure function should return the data as an array of SubjectModels
+            if (Array.isArray(data) && data.every(isSubjectModel)) {
+                return data;
+            }
+            return undefined;
+        },
+        []
+    );
+
+    if (fetchResult.status === FetchStatus.Success) {
+        const subjectListItems = fetchResult.payload.map((subject) => (
+            <ul key={subject.id} className={homeStyles.columnContainer}>
+                <h2>{subject.subject_name}</h2>
+            </ul>
+        ))
+        return (
+            <ul>
+                {subjectListItems}
+            </ul>
+        );
+    } else if (fetchResult.status === FetchStatus.Failure) {
+        // Notify user that the subjects list couldn't be fetched
+        return (
+            <p>Could not fetch subjects! Reason: {fetchResult.reason}</p>
+        )
+    } else {
+        // Notify user that the subjects list is currently being fetched
+        return (
+            <p>Fetching subjects list...</p>
+        )
+    }
+}
+
 const Home: React.FC = () => {
+    const [subjectName, setSubjectName] = useState<string | undefined>(undefined);
+
     return (
         <div className={styles.content}>
             <h3>Home</h3>
-            <div className={homeStyles.overallContainer}>
-                <div className={styles.container}>
-                    <h1>Your Stats</h1>
-                </div>
-                <div className={styles.container}>
-                    <h1>Available Subjects</h1>
+            <div className={homeStyles.columnContainer}>
+                <div>
+                    <SubjectSelector callback={setSubjectName}/>
                 </div>
                 <div className={homeStyles.buttonContainer}>
                     <Button className={homeStyles.createButton} href="/login" variant="contained" color="primary" startIcon={<CreateIcon />}>Create a game</Button>
