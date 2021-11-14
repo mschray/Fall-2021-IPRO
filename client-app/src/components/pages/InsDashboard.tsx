06572e3e35f9ca2e4c7876c23c6123b7413a3d96
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2021-11-13 20:05:28 -0600

    Initialize an interface for instructor dashboard

--- client-app/src/components/pages/InsDashboard.tsx
+++ client-app/src/components/pages/InsDashboard.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 
+interface InstructorState {
+    email: string,
+    courses: Array<String>
+}
 const InsDashboard: React.FC = () => {
+    const initialState: InstructorState ={
+        email: "",
+        courses: []
+    }
+    
     return (
         <div>
             Hello instructor
