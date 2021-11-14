916b044d44b97a1c6e5dd6d11a59cc9dd17524a1
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2021-11-13 21:19:43 -0600

    Created an UL of courses in Instructor Dashboard

--- client-app/src/components/pages/InsDashboard.tsx
+++ client-app/src/components/pages/InsDashboard.tsx
@@ -1,3 +1,4 @@
+import Button from "@mui/material/Button";
 import React from "react";
 import styles from "./Page.module.scss";
 
@@ -5,15 +6,30 @@ interface InstructorState {
     email: string,
     courses: Array<String>
 }
+
 const InsDashboard: React.FC = () => {
     const initialState: InstructorState ={
         email: "",
         courses: []
     }
 
+    initialState.courses = ["algebra1", "algebra2"]
+    const menuButtons = initialState.courses.map(course => (
+        <ul>
+            <li>
+            <Button>
+                {course}
+            </Button>
+            </li>
+        </ul>
+    ));
     return (
         <div className={styles.content}>
-            Hello instructor
+            <h3>Hello instructor</h3>
+            <p>
+                Courses
+                {menuButtons}
+            </p>
         </div>
     );
 };
