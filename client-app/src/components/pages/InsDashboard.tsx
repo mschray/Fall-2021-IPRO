18aec98525918a26219658112e749b2d9a3f017b
khiemnguyen22 <55027703+khiemnguyen22@users.noreply.github.com>
2021-11-13 18:48:21 -0600

    Create Instructor Dashboard page

---
+++ client-app/src/components/pages/InsDashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InsDashboard: React.FC = () => {
+    return (
+        <div>
+            Hello instructor
+        </div>
+    );
+};
+
+export default InsDashboard;
