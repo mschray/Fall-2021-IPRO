8be83318806bfe74adbf6761a41c7b99a7037c46
Dan Tiberi <danny.tiberi@gmail.com>
2021-11-15 16:45:24 -0600

    Fixed UserSignIn to work with new DB changes and Tools function.
    - Major code cleanup needed.

--- JebraAzureFunctions/JebraAzureFunctions/UserSignIn.cs
+++ JebraAzureFunctions/JebraAzureFunctions/UserSignIn.cs
@@ -29,9 +29,9 @@ namespace JebraAzureFunctions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string courseCode = req.Query["courseCode"];
+            int courseCode = int.Parse(req.Query["courseCode"]);
             string userEmail = req.Query["userEmail"];
-
+            Console.WriteLine("1");
             /*
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
@@ -48,9 +48,10 @@ namespace JebraAzureFunctions
              */
 
             string courseIdS = Tools.ExecuteQueryAsync($"SELECT id FROM course WHERE code={courseCode}").GetAwaiter().GetResult();
+            Console.WriteLine("2");
             //dynamic data = JsonConvert.DeserializeObject(courseIdS.Substring(1, courseIdS.Length - 2));//Removes [] from ends.
             int courseId = Tools.GetIdFromResponse(courseIdS);
-
+            Console.WriteLine("3");
             //string userIdS = Tools.ExecuteQueryAsync($"SELECT id FROM app_user WHERE email='{userEmail}'").GetAwaiter().GetResult();
             string userIdS = Tools.ExecuteQueryAsync($@"
                 IF EXISTS
@@ -65,38 +66,53 @@ namespace JebraAzureFunctions
                 
                 SELECT id FROM app_user WHERE email='{userEmail}'
             ").GetAwaiter().GetResult();
+
+            Console.WriteLine("4");
             //data = JsonConvert.DeserializeObject(userIdS.Substring(1, userIdS.Length - 2));//Removes [] from ends.
             //int userId = data?.id;
             int userId = Tools.GetIdFromResponse(userIdS);
-
-            string instructorIds = Tools.ExecuteQueryAsync($"SELECT instructor_id FROM course_assignment WHERE course_id={courseId} AND user_id IS NULL").GetAwaiter().GetResult();
+            Console.WriteLine("5");
+            string instructorIds = Tools.ExecuteQueryAsync($"SELECT instructor_id AS id FROM course_assignment WHERE course_id={courseId} AND user_id IS NULL").GetAwaiter().GetResult();
             //data = JsonConvert.DeserializeObject(instructorIds.Substring(1, instructorIds.Length - 2));//Removes [] from ends.
             //int instructorId = data?.instructor_id;
+            Console.WriteLine("6");
+            Console.WriteLine(instructorIds);
+            //[{"instructor_id":1}]
             int instructorId = Tools.GetIdFromResponse(instructorIds);
-
+            Console.WriteLine("7");
             //Console.WriteLine($"courseId:{courseId}, userId:{userId}, instructorId:{instructorId}");
 
             //Get stage.id
             int stageId = -1;
+            /*
             string stageIdS = Tools.ExecuteQueryAsync($@"
-                SELECT TOP 1 stage_event_join.stage_id
+                SELECT TOP 1 stage_event_join.stage_id AS id
                 FROM stage_event_join 
                 INNER JOIN course ON stage_event_join.course_id = course.id
                 WHERE course.code = {courseCode};
             ").GetAwaiter().GetResult();
-            Console.WriteLine(stageIdS);
+            */
+            string stageIdS = Tools.ExecuteQueryAsync($@"
+                SELECT stage_id AS id
+                FROM course 
+                WHERE id = {courseId};
+            ").GetAwaiter().GetResult();
+
+            Console.WriteLine("8");
             //data = JsonConvert.DeserializeObject(stageIdS.Substring(1, stageIdS.Length - 2));//Removes [] from ends.
             //stageId = data?.stage_id;
+            Console.WriteLine(stageIdS);
             stageId = Tools.GetIdFromResponse(stageIdS);
-
+            Console.WriteLine("9");
             await Tools.ExecuteNonQueryAsync($"INSERT INTO course_assignment (user_id, course_id, instructor_id) VALUES({userId},{courseId},{instructorId})");
-
+            Console.WriteLine("10");
             UserSignInResponseModel res = new UserSignInResponseModel();
             res.courseId = courseId;
             res.userId = userId;
             res.instructorId = instructorId;
             res.stageId = stageId;
-       
+
+            Console.WriteLine("11");
             return new OkObjectResult(res);
         }
     }
