17602d79c887fd450378525ddb9662278bc430dc
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2021-11-15 14:54:11 -0600

    Fix Header.tsx indentation

--- client-app/src/components/Header.tsx
+++ client-app/src/components/Header.tsx
@@ -17,7 +17,7 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 
 import { Link as RouterLink } from "react-router-dom";
 
-  const headersData = [
+const headersData = [
     {
         label: "Home",
         href: "/",
@@ -34,151 +34,150 @@ import { Link as RouterLink } from "react-router-dom";
         label: "Play",
         href: "/play",
     },
-  ];
+];
   
-  const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(() => ({
     header: {
-      backgroundColor: "#262F34",
-      filter: "drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.25))",
-      paddingRight: "79px",
-      paddingLeft: "5rem",
-      "@media (max-width: 900px)": {
+        backgroundColor: "#262F34",
+        filter: "drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.25))",
+        paddingRight: "79px",
         paddingLeft: "5rem",
-      },
-      borderRadius: "15px",
-      display: "flex",
-      flexFlow:"row wrap",
-      justifyContent: "space-between",
-
-      position: "relative",
-      height: "5rem"
+        "@media (max-width: 900px)": {
+            paddingLeft: "5rem",
+        },
+        borderRadius: "15px",
+        display: "flex",
+        flexFlow:"row wrap",
+        justifyContent: "space-between",
+        position: "relative",
+        height: "5rem"
     },
     menuButton: {
-      fontFamily: "Open Sans, sans-serif",
-      fontWeight: 700,
-      size: "18px",
-      marginLeft: "38px",
+        fontFamily: "Open Sans, sans-serif",
+        fontWeight: 700,
+        size: "18px",
+        marginLeft: "38px",
     },
     toolbar: {
-      display: "flex",
-      justifyContent: "space-between",
+        display: "flex",
+        justifyContent: "space-between",
     },
     drawerContainer: {
-      padding: "20px 30px",
+        padding: "20px 30px",
     },
-  }));
+}));
   
-  export default function Header() {
+export default function Header() {
     const { header, menuButton, toolbar, drawerContainer } = useStyles();
-  
+
     const [state, setState] = useState({
-      mobileView: false,
-      drawerOpen: false,
+        mobileView: false,
+        drawerOpen: false,
     });
-  
+
     const { mobileView, drawerOpen } = state;
-  
+
     useEffect(() => {
-      const setResponsiveness = () => {
+        const setResponsiveness = () => {
         return window.innerWidth < 900
-          ? setState((prevState) => ({ ...prevState, mobileView: true }))
-          : setState((prevState) => ({ ...prevState, mobileView: false }));
-      };
-  
-      setResponsiveness();
-  
-      window.addEventListener("resize", () => setResponsiveness());
-  
-      return () => {
-        window.removeEventListener("resize", () => setResponsiveness());
-      };
+            ? setState((prevState) => ({ ...prevState, mobileView: true }))
+            : setState((prevState) => ({ ...prevState, mobileView: false }));
+        };
+
+        setResponsiveness();
+
+        window.addEventListener("resize", () => setResponsiveness());
+
+        return () => {
+            window.removeEventListener("resize", () => setResponsiveness());
+        };
     }, []);
-  
+
     const displayDesktop = () => {
-      return (
-        <Toolbar className={toolbar}>
-          <JebraSVG className={styles.logoDesktop} />
-          <div>{getMenuButtons()}</div>
-        </Toolbar>
-      );
+        return (
+            <Toolbar className={toolbar}>
+                <JebraSVG className={styles.logoDesktop} />
+                <div>{getMenuButtons()}</div>
+            </Toolbar>
+        );
     };
-  
+
     const displayMobile = () => {
-      const handleDrawerOpen = () =>
-        setState((prevState) => ({ ...prevState, drawerOpen: true }));
-      const handleDrawerClose = () =>
-        setState((prevState) => ({ ...prevState, drawerOpen: false }));
-  
-      return (
-        <Toolbar>
-          <IconButton
-            {...{
-              edge: "start",
-              color: "inherit",
-              "aria-label": "menu",
-              "aria-haspopup": "true",
-              onClick: handleDrawerOpen,
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-  
-          <Drawer
-            {...{
-              anchor: "left",
-              open: drawerOpen,
-              onClose: handleDrawerClose,
-            }}
-          >
-            <div className={drawerContainer}>{getDrawerChoices()}</div>
-          </Drawer>
-  
-          <JebraSVG className={styles.logoMobile} />
-        </Toolbar>
-      );
+        const handleDrawerOpen = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: true }));
+        const handleDrawerClose = () =>
+            setState((prevState) => ({ ...prevState, drawerOpen: false }));
+
+        return (
+            <Toolbar>
+                <IconButton
+                    {...{
+                        edge: "start",
+                        color: "inherit",
+                        "aria-label": "menu",
+                        "aria-haspopup": "true",
+                        onClick: handleDrawerOpen,
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+
+                <Drawer
+                    {...{
+                        anchor: "left",
+                        open: drawerOpen,
+                        onClose: handleDrawerClose,
+                    }}
+                >
+                    <div className={drawerContainer}>{getDrawerChoices()}</div>
+                </Drawer>
+
+                <JebraSVG className={styles.logoMobile} />
+            </Toolbar>
+        );
     };
-  
+
     const getDrawerChoices = () => {
-      return headersData.map(({ label, href }) => {
-        return (
-          <Link
-            {...{
-              component: RouterLink,
-              to: href,
-              color: "inherit",
-              style: { textDecoration: "none" },
-              key: label,
-            }}
-          >
-            <MenuItem>{label}</MenuItem>
-          </Link>
-        );
-      });
+        return headersData.map(({ label, href }) => {
+            return (
+                <Link
+                    {...{
+                        component: RouterLink,
+                        to: href,
+                        color: "inherit",
+                        style: { textDecoration: "none" },
+                        key: label,
+                    }}
+                >
+                    <MenuItem>{label}</MenuItem>
+                </Link>
+            );
+        });
     };
-  
+
     const getMenuButtons = () => {
-      return headersData.map(({ label, href }) => {
-        return (
-          <Button
-            {...{
-              key: label,
-              color: "inherit",
-              to: href,
-              component: RouterLink,
-              className: menuButton,
-            }}
-          >
-            {label}
-          </Button>
-        );
-      });
+        return headersData.map(({ label, href }) => {
+            return (
+                <Button
+                    {...{
+                        key: label,
+                        color: "inherit",
+                        to: href,
+                        component: RouterLink,
+                        className: menuButton,
+                    }}
+                >
+                    {label}
+                </Button>
+            );
+        });
     };
-  
+
     return (
-      <header>
-        <AppBar className={header}>
-          {mobileView ? displayMobile() : displayDesktop()}
-        </AppBar>
-      </header>
+        <header>
+            <AppBar className={header}>
+                {mobileView ? displayMobile() : displayDesktop()}
+            </AppBar>
+        </header>
     );
-  }
+}
