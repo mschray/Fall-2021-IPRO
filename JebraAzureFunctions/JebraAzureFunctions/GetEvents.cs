7799f8e054e7ed0f4302d46ec32a797e20ba01bb
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2021-11-16 09:53:29 -0600

    Fix bugs, add check for new stage at beginning, update course's stage_id

--- JebraAzureFunctions/JebraAzureFunctions/GetEvents.cs
+++ JebraAzureFunctions/JebraAzureFunctions/GetEvents.cs
@@ -46,15 +46,27 @@ namespace JebraAzureFunctions
             ///name = name ?? data?.name;
             ///   
 
+            // First, check if the course has a new stage!
+            string currentStageString = Tools.ExecuteQueryAsync($"SELECT TOP 1 stage_id FROM course WHERE code={courseCode};").GetAwaiter().GetResult();
+            currentStageString = currentStageString.Substring(1, currentStageString.Length - 2);
+            dynamic currentStageData = JsonConvert.DeserializeObject(currentStageString);
+            int currentStageId = currentStageData?.stage_id;
+
+            // If the course has a new stage, return an EndOfStage response
+            if (currentStageId.ToString() != stage)
+            {
+                return new OkObjectResult("{\"EndOfStage\": true, \"NewStage\": " + currentStageId.ToString() + "}");
+            }
+
             string hpIdString = Tools.ExecuteQueryAsync($"SELECT max_hp FROM stage WHERE id={stage}").GetAwaiter().GetResult();
             //[{"id":2}]
             hpIdString = hpIdString.Substring(1, hpIdString.Length - 2);
             //{"id":2}
             dynamic data = JsonConvert.DeserializeObject(hpIdString);
-            int maxHp = data?.id;
+            int maxHp = data?.max_hp;
 
             string command = @$"
-            SELECT stage_event.inflicted_hp, stage_event.was_correct, stage_event.event_time FROM stage_event
+            SELECT stage_event.id, stage_event.inflicted_hp, stage_event.was_correct, stage_event.event_time FROM stage_event
             INNER JOIN stage_event_join ON stage_event.id = stage_event_join.stage_event_id
             INNER JOIN course ON course.code = {courseCode}
             WHERE stage_event_join.course_id = course.id AND stage_event_join.stage_id = {stage}
@@ -65,9 +77,9 @@ namespace JebraAzureFunctions
             subjectIdString = subjectIdString.Substring(1, subjectIdString.Length - 2);
             //{"id":2}
             data = JsonConvert.DeserializeObject(subjectIdString);
-            int subjectId = data?.id;
+            int subjectId = data?.subject_id;
 
-            dynamic responseMessage = Tools.ExecuteQueryAsync(command).GetAwaiter().GetResult();
+            dynamic responseMessage = JsonConvert.DeserializeObject(Tools.ExecuteQueryAsync(command).GetAwaiter().GetResult());
 
             List<StageEventModel> events = Tools.JsonEventsToModelArray(responseMessage);
 
@@ -80,20 +92,23 @@ namespace JebraAzureFunctions
             if(currentHp <= 0)
             {
                 /*
-                 * - Delete current stage
                  * - Create new stage
+                 * - Update course with new stage_id
+                 * - Delete old stage
                  * - Ret new stage_id
                  */
-                Tools.ExecuteNonQueryAsync($"DELETE FROM stage WHERE id={stage}").GetAwaiter().GetResult();
-
                 string newStageIdString = Tools.ExecuteQueryAsync($@"INSERT INTO stage OUTPUT INSERTED.id VALUES(100, 'punisher',{subjectId})").GetAwaiter().GetResult();
                 //[{"id":2}]
-                newStageIdString = newStageIdString.Substring(1, subjectIdString.Length - 2);
+                newStageIdString = newStageIdString.Substring(1, newStageIdString.Length - 2);
                 //{"id":2}
                 data = JsonConvert.DeserializeObject(newStageIdString);
                 int newStageId = data?.id;
 
-                responseMessage = "{'EndOfStage': true, 'NewStage': " + newStageId + "}";
+                Tools.ExecuteNonQueryAsync($"UPDATE course SET stage_id={newStageId} WHERE code={courseCode}").GetAwaiter().GetResult();
+
+                Tools.ExecuteNonQueryAsync($"DELETE FROM stage WHERE id={stage}").GetAwaiter().GetResult();
+
+                responseMessage = "{\"EndOfStage\": true, \"NewStage\": " + newStageId + "}";
             }
             return new OkObjectResult(responseMessage);
         }
