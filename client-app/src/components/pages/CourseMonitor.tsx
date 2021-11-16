fd356144cfc3ba3ed01a7731b5907132315a06c4
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2021-11-16 10:48:35 -0600

    Initialize CourseMonitor.tsx page

---
+++ client-app/src/components/pages/CourseMonitor.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./Page.module.scss"
+
+const CourseMonitor: React.FC = () => {
+    return (
+        <div className={styles.content}>
+            <h3>Course Monitor</h3>
+        </div>
+    )
+}
+
+export default CourseMonitor;
