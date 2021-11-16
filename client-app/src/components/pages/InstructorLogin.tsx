6083a9faf2e57d10e11f2437a46dd1ce54270183
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2021-11-16 10:48:21 -0600

    Add PUT request to InstructorLogin

--- client-app/src/components/pages/InstructorLogin.tsx
+++ client-app/src/components/pages/InstructorLogin.tsx
@@ -3,33 +3,64 @@ import React, { useState } from "react";
 import styles from "./Page.module.scss"
 import useForm from "hooks/useForm";
 
+import getAzureFunctions from "getAzureFunctions";
+
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
-import InstructorModel from "models/InstructorModel"; 
+import InstructorModel, { isInstructorModel } from "models/InstructorModel"; 
 
 interface InstructorLoginProps {
     onLogin: (data: InstructorModel) => void
 }
 
 interface InstructorLoginFormState {
-    email: string,
+    username: string,
     password: string,
 };
 
 const InstructorLogin: React.FC<InstructorLoginProps> = props => {
+    // login error message state
+    const [loginErrorState, setLoginErrorState] = useState<string | undefined>(undefined);
+
     const initialState: InstructorLoginFormState = {
-        email: "",
+        username: "",
         password: "",
     };
     const [visible, setVisible] = useState(false);
 
     // a submit function that will execute upon form submission
     async function loginUserCallback() {
-        // send "values" to database
+        const url = new URL(getAzureFunctions().InstructorLogin);
+
+        const requestInfo: RequestInit = {
+            method: "PUT",
+            body: JSON.stringify({
+                username: formState.username,
+                pass: formState.password
+            })
+        };
+
+        fetch(url.toString(), requestInfo)
+            .then(response => response.json())
+            .then(json => {
+                console.log(json);
+                if (Array.isArray(json) && isInstructorModel(json[0])) {
+                    // Fire onLogin callback with the InstructorModel data
+                    props.onLogin(json[0]);
+                } else {
+                    // Data doesn't conform to InstructorModel
+                    setLoginErrorState("Unexpected data returned from backend. Check the console.");
+                }
+            })
+            .catch(err => {
+                // Error occurred furing PUT request
+                console.error(err);
+                setLoginErrorState("An error occured during login. Check the console.");
+            });
     }
 
     const toggleVisibility = () => {
@@ -47,7 +78,7 @@ const InstructorLogin: React.FC<InstructorLoginProps> = props => {
             <h3>Instructor Log In</h3>
             <form onSubmit={onFormSubmit}>
                 <div>
-                    <TextField name="email" sx={{ m: 1, width: '25ch' }} required label="Email" value={formState.email} placeholder="Email" type="email" onChange={onFormChange} />
+                    <TextField name="username" sx={{ m: 1, width: '25ch' }} required label="Username" value={formState.username} placeholder="Username" type="text" onChange={onFormChange} />
                 </div>
                 <br/>
                 <div>                
@@ -66,6 +97,11 @@ const InstructorLogin: React.FC<InstructorLoginProps> = props => {
                     />
                 </div>
                 <br/>
+                {
+                    loginErrorState !== undefined
+                        ? <p>{loginErrorState}</p>
+                        : null
+                }
                 <input type="submit" value="Submit" />
             </form>
             <p>New to Jebra? <a href="/Signup">Create an account</a></p>
