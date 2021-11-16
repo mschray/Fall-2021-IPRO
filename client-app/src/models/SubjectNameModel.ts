e106926d5b2b67083061b9bdf96328aff664fee9
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2021-11-16 05:43:25 -0600

    Add SubjectNameModel.ts

---
+++ client-app/src/models/SubjectNameModel.ts
@@ -0,0 +1,14 @@
+// Interface for Subject Name JSON data returned from backend
+export default interface SubjectNameModel {
+    subject_name: string
+}
+
+// Type guard for validating that data returned from the backend contains the expected fields
+export function isSubjectNameModel(data: any): data is SubjectNameModel {
+    if (typeof data !== "object" || Array.isArray(data) || data === null) {
+        return false;
+    }
+
+    const subjectData = data as SubjectNameModel;
+    return typeof subjectData.subject_name === "string"
+}
