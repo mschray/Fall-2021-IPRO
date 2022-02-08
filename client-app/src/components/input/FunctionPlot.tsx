149e6e13233c672db54eb0704a782267167ba0b3
knguyen22 <knguyen22@hawk.iit.edu>
2022-02-08 12:25:52 -0600

    adjust functionplot to be more general

--- client-app/src/components/input/FunctionPlot.tsx
+++ client-app/src/components/input/FunctionPlot.tsx
@@ -5,44 +5,30 @@ interface FunctionProperties{
     function: string
 }
 
-const FunctionPlot: React.FC = () => {
-    const [functionValue, setFunctionValue] = useState<string>("");
+const FunctionPlot: React.FC<FunctionProperties> = (props) => {
+
 
     let contentsBounds = document.body.getBoundingClientRect();
-    let width = 800;
-    let height = 500;
-    let ratio = contentsBounds.width / width;
-    width *= ratio;
-    height *= ratio;
+    
+    let width = contentsBounds.width * 0.3
+    let height = width
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFunctionValue(event.target.value)
-    }
-    const handleSubmit = () => {
-        functionPlot({
-            target: "#root",
-            width,
-            height,
-            yAxis: { domain: [-1, 9] },
-            grid: true,
-            data: [
-              {
-                fn: functionValue,
-          
-              }
-            ]
-          });
-    }
+    functionPlot({
+        target: "#root",
+        width ,
+        height,
+        yAxis: { domain: [-5, 5] },
+        xAxis: { domain: [-5, 5] },
+        grid: true,
+        data: [
+        {
+            fn: props.function,
+        }
+        ]
+    })
 
     return(
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                Input function:
-                    <input type="text" />
-                </label>
-                <input type="submit" value="Submit" onChange ={handleChange}/>
-            </form>
         </div>
     );
 };
