67b43e095d9e781bf4cbf267b99451b3502f931a
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2022-02-16 23:52:18 -0600

    Add SubjectName type

--- client-app/src/models/QuestionModel.ts
+++ client-app/src/models/QuestionModel.ts
@@ -1,10 +1,22 @@
+// Subjects type
+type SubjectName = "Simplify Exponents"
+             | "Simplify Exponents 2"
+             | "Simplify Square Roots"
+             | "Factorials"
+             | "Cartesian Coordinates"
+             | "Single Variable"
+             | "System of Equations"
+             | "Quadratic Roots"
+             | "Trig Functions"
+             | "Inverse Trig Functions"
+
 // Interface for Question JSON data returned from backend
 export default interface QuestionModel {
     id: number,
     answer_a: string,
     answer_b: string | null,
     question: string,
-    subject_name: string,
+    subject_name: SubjectName,
     is_json: boolean
 }
 
