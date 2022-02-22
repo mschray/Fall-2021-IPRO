1fe4469b664a4288b075c816ddbfce740b4e6221
Nam Nguyen <nnguyen17@hawk.iit.edu>
2022-02-22 17:29:40 -0600

    Revert "adjust styling for mobile header"
    This reverts commit bcf192bf995d2795ed1cd86c508ce0b8e90caa8c.

--- client-app/src/components/Header.tsx
+++ client-app/src/components/Header.tsx
@@ -27,59 +27,6 @@ const headersData = [
         href: "/play",
     },
 ];
-<<<<<<< Updated upstream
-=======
-  
-const useStyles = makeStyles(() => ({
-    header: {
-        backgroundColor: "#262F34",
-        filter: "drop-shadow(4px 4px 1px rgba(0, 0, 0, 0.25))",
-        borderRadius: "15px",
-        display: "flex",
-        flexFlow: "row wrap",
-        position: "relative",
-        height: "5rem",
-        paddingLeft: "5rem",
-        paddingRight: "5rem",
-        justifyContent: "space-between",
-        "@media (max-width: 900px)": {
-            paddingLeft: "0rem",
-            paddingRight: "2rem",
-            justifyContent: "start",
-        },
-    },
-    menuButton: {
-        fontFamily: "Open Sans, sans-serif",
-        fontWeight: 700,
-        size: "18px",
-        marginLeft: "38px",
-    },
-    toolbar: {
-        display: "flex",
-        justifyContent: "space-evenly",
-    },
-    drawerContainer: {
-        padding: "20px 30px",
-    },
-}));
-  
-export default function Header() {
-    const { header, menuButton, toolbar, drawerContainer } = useStyles();
-
-    const [state, setState] = useState({
-        mobileView: false,
-        drawerOpen: false,
-    });
-
-    const { mobileView, drawerOpen } = state;
-
-    useEffect(() => {
-        const setResponsiveness = () => {
-        return window.innerWidth < 900
-            ? setState((prevState) => ({ ...prevState, mobileView: true }))
-            : setState((prevState) => ({ ...prevState, mobileView: false }));
-        };
->>>>>>> Stashed changes
 
 
 const Header: React.FC = () => {
