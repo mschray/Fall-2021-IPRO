ed5bbd0913c2b9e63095de7fb3735023b954bd00
Tommy Vadakumchery <t.vadakumchery1@gmail.com>
2022-03-29 17:59:49 -0500

    Remove input field from Question

--- client-app/src/components/Question.tsx
+++ client-app/src/components/Question.tsx
@@ -2,14 +2,7 @@ import React, { useState } from "react";
 
 import styles from "./Question.module.scss";
 
-import useForm from "hooks/useForm";
 import QuestionModel from "models/QuestionModel";
-import { isRightTriangleTrigModel } from "models/questions/RightTriangleTrigModel";
-
-import Latex from "react-latex";
-
-import TextField from "@mui/material/TextField";
-import RightTriangle from "components/diagrams/RightTriangle";
 import determineQuestionFormatting from "determineQuestionFormatting";
 import NumPad from "components/input/numpad";
 
@@ -20,42 +13,10 @@ interface QuestionProperties {
     questionNumber?: number
 }
 
-// Interface for answer form
-interface AnswerFormState {
-    answer: string
-}
-
 enum AnswerResult {
     Correct,
     Incorrect,
     NoAnswersYet
-}
-
-function hasNumericAnswer(question: QuestionModel): boolean {
-    if (question.answer_b !== null) {
-        return !isNaN(Number(question.answer_a)) && !isNaN(Number(question.answer_b));
-    }
-    return !isNaN(Number(question.answer_a));
-}
-
-function getDecimalPlaces(answerOrQuestion: string | QuestionModel): number {
-    if (typeof answerOrQuestion === "string") {
-        const parts = answerOrQuestion.split(".");
-
-        if (parts.length < 2)
-            return 0;
-
-        return parts[1].length;
-    } else {
-        if (answerOrQuestion.answer_b !== null) {
-            return Math.max(
-                getDecimalPlaces(answerOrQuestion.answer_a),
-                getDecimalPlaces(answerOrQuestion.answer_b)
-            );
-        } else {
-            return getDecimalPlaces(answerOrQuestion.answer_a);
-        }
-    }
 }
 
 const Question: React.FC<QuestionProperties> = (props) => {
@@ -66,59 +27,20 @@ const Question: React.FC<QuestionProperties> = (props) => {
         if (answer === props.questionData.answer_a || answer === props.questionData.answer_b) { //If correct answer
             setLastAnswerResult(AnswerResult.Correct);
             props.onSolve(props.questionData, 1);
-            // Clear input field
-            setFormState({ answer: "" });
         } else { //If not correct answer
             props.onSolve(props.questionData, 0);
             setLastAnswerResult(AnswerResult.Incorrect);
         }
     }
 
-    // Callback to be fired when the Submit button is entered
-    async function submitAnswerCallback(state: AnswerFormState) {
-        submitAnswer(state.answer);
-    }
-    
-    // Default state for the Answer form
-    const initialState: AnswerFormState = {
-        answer: ""
-    }
-    
-    // Create a Form state using the answer submission callback and the default state above
-    const [formState, onFormChange, onFormSubmit, setFormState] = useForm(
-        submitAnswerCallback,
-        initialState
-    );
-
     const questionNumberLabel = (props.questionNumber !== undefined) ? `(${props.questionNumber})` : null;
 
-    const [questionStatement, diagram] = determineQuestionFormatting(props.questionData);
+    const [questionStatement, _] = determineQuestionFormatting(props.questionData);
 
     return (
         <>
             <div style={{gridArea: "question"}}>
                 <p>{questionNumberLabel} {questionStatement}</p>
-                <form onSubmit={onFormSubmit}>
-                    <TextField
-                        required
-                        name="answer"
-                        id="answer"
-                        sx={{ m: 1, width: '25ch' }}
-                        label="Answer"
-                        value={formState.answer}
-                        placeholder="Answer"
-                        type={hasNumericAnswer(props.questionData) ? "number" : "text"}
-                        inputProps={
-                            hasNumericAnswer(props.questionData)
-                                ? { step: Math.pow(10, -getDecimalPlaces(props.questionData)) }
-                                : {}
-                        }
-                        autoComplete="off"
-                        onChange={onFormChange}
-                    />
-                    <br />
-                    <input type="submit" />
-                </form>
                 {
                     (lastAnswerResult === AnswerResult.Correct)
                         ? <p className={styles.correct}>That was correct!</p>
