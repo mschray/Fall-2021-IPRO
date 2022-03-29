3d4491a1f59182d9d89fa50e35f7580535ec160c
knguyen22 <knguyen22@hawk.iit.edu>
2022-03-29 14:00:57 -0500

    Update PieChart component

--- client-app/src/components/diagrams/PieCharts.tsx
+++ client-app/src/components/diagrams/PieCharts.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Tooltip } from 'recharts';
 
 interface PieChartProperties {
     data: Array<Object>,
@@ -8,7 +8,7 @@ interface PieChartProperties {
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }: any) => {
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index, payload }: any) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
@@ -24,20 +24,21 @@ const PieCharts: React.FC<PieChartProperties> = (props) => {
     const data = props.data
     return (
         <PieChart width={400} height={400}>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-          >
+            <Pie
+              dataKey="value"
+              data={props.data}
+              cx={200}
+              cy={200}
+              outerRadius={80}
+              labelLine={false}
+              fill="#8884d8"
+              label = {renderCustomizedLabel}
+            >
             {data.map((entry, index) => (
               <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
             ))}
-          </Pie>
+            </Pie>
+            <Tooltip />
         </PieChart>
     );
 };
